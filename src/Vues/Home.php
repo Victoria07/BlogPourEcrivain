03e83113e1012387c6f22affe40163a1a88a097c
Victoria07 <vic.iber16@gmail.com>
2018-09-26 16:43:06 +0200

    Correction des liens dans la page apropos et bilets du thème bootstrap

--- src/Vues/Home.php
+++ src/Vues/Home.php
@@ -1,3 +1,21 @@
-<?php 
-
+<?php
 ?>
+
+<DOCTYPE! html> 
+<html> 
+    <head>
+        <meta charset='utf-8'/> 
+        <link href="../../assets/boostrap/css/bootstrap.min.css" rel="stylesheet"> 
+
+
+
+    </head> 
+
+
+    <body>
+
+
+    </body> 
+
+</html> 
+
